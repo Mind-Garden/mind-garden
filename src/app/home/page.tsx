cc6e3e1dc806622a8f694697aa289cd76ef2df74
Hassan <hkhan701personal@gmail.com>
2025-02-27 20:42:09 -0600

    Update header opacity style in Home component to use tailwind css

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -29,7 +29,7 @@ export default async function Home() {
 
       <main className="flex-1 container mx-auto px-4 py-8">
       <div className="mb-8 text-center">
-          <h1 className="text-3xl font-bold mb-2 " style={{ opacity: 0.5}}>
+          <h1 className="text-3xl font-bold mb-2 opacity-50">
             Welcome to Mind Garden, {profileData?.first_name}!
           </h1>
           <p className="text-lg text-muted-foreground">
