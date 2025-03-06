7035d3b364759e780b1b076f8dd6fc470774a75d
jiminryuu <144750019+jiminryuu@users.noreply.github.com>
2025-03-06 17:48:39 -0600

    Fix Merge Conflict

--- src/actions/data-visualization.ts
+++ src/actions/data-visualization.ts
@@ -25,32 +25,6 @@ export async function selectMoodDataByDateRange(
     user_id: userId,
   };
 
-  const { data, error } = await selectData(table, conditions, columns, startDate, endDate);
-
-  if (error) {
-    console.error("Error fetching mood data:", error.message);
-    return { error: error.message };
-  }
-
-  return { data }
-}
-
-/**
- * 
- * @param userId - The user ID whose mood frequency needs to be fetched
- * @returns a list of dictionaries with scale_rating and count
- */
-export async function selectMoodFrequency(userId: string, lastMonthDate: string, todaysDate: string) {
-  const supabase = getSupabaseClient();
-
-  //retrieve mood frequency data from the database for the past month starting from today
-  const { data, error } = await supabase.rpc('get_mood_count_by_user',
-    {
-      user_id_param: userId,
-      start_date_param: lastMonthDate,
-      end_date_param: todaysDate
-    });
-=======
   const { data, error } = await selectData(
     table,
     conditions,
@@ -67,20 +41,56 @@ export async function selectMoodFrequency(userId: string, lastMonthDate: string,
   return { data };
 }
 
-export async function selectSleepDataByDateRange(userId: string, startDate: string, endDate: string) {
+/**
+ *
+ * @param userId - The user ID whose mood frequency needs to be fetched
+ * @returns a list of dictionaries with scale_rating and count
+ */
+export async function selectMoodFrequency(
+  userId: string,
+  lastMonthDate: string,
+  todaysDate: string,
+) {
+  const supabase = getSupabaseClient();
+
+  //retrieve mood frequency data from the database for the past month starting from today
+  const { data, error } = await supabase.rpc('get_mood_count_by_user', {
+    user_id_param: userId,
+    start_date_param: lastMonthDate,
+    end_date_param: todaysDate,
+  });
+
+  if (error) {
+    console.error('Error fetching mood data:', error.message);
+    return { error: error.message };
+  }
+
+  return { data };
+}
+
+export async function selectSleepDataByDateRange(
+  userId: string,
+  startDate: string,
+  endDate: string,
+) {
   const table = 'sleep_entries';
   const columns = ['entry_date', 'start', 'end'];
   const conditions = {
     user_id: userId,
   };
 
-  const { data, error } = await selectData(table, conditions, columns, startDate, endDate);
+  const { data, error } = await selectData(
+    table,
+    conditions,
+    columns,
+    startDate,
+    endDate,
+  );
 
   if (error) {
-    console.error("Error fetching sleep data:", error.message);
+    console.error('Error fetching sleep data:', error.message);
     return { error: error.message };
   }
 
-  return { data }
+  return { data };
 }
-
