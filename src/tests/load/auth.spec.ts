d60dd800dfe09cb99048ccd3733cea5f7035305b
Manisha Gurukumar <ullasmani16@gmail.com>
2025-03-26 00:16:07 -0500

    deleted unused files

--- src/tests/load/auth.spec.ts
+++
@@ -1,7 +0,0 @@
-// import { tPAge, est } from '@playwright/test';
-
-// import { testLogin } from './commands/login';
-
-// test('test', async ({ page }) => {
-//   await testLogin({ page });
-// });
