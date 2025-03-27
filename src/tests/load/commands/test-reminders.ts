b43c082eb892ed5915df377ccef342ddd1d2efd5
orca277 <killerwhale2779@protonmail.com>
2025-03-27 18:26:39 -0500

    Update test-reminders.ts

--- src/tests/load/commands/test-reminders.ts
+++ src/tests/load/commands/test-reminders.ts
@@ -3,24 +3,27 @@ import { Page } from '@playwright/test';
 let time = 1;
 
 async function testReminders(page: Page) {
+  const workerNumber = Math.floor(Math.random() * 10);
+  const email = `load${workerNumber}@test.com`;
+
   await page.goto('https://mindgarden.vercel.app/');
   await page.waitForTimeout(2000);
   await page.getByRole('button', { name: 'Get Started' }).click();
-  await page.getByRole('textbox', { name: 'Email' }).fill('load@test.com');
+  await page.getByRole('textbox', { name: 'Email' }).fill(email);
   await page.getByRole('textbox', { name: 'Password' }).fill('loadtest');
   await page.getByRole('button', { name: 'Log in' }).click();
-  await page
-    .getByRole('main')
-    .getByRole('button', { name: 'Reminders' })
-    .click();
-  await page.getByRole('button', { name: String(time), exact: true }).click();
-  time++;
-  if (time === 12) {
-    time = 1;
-  }
-  await page.getByRole('button', { name: 'Save Reminders' }).click();
+  await page.goto('https://mindgarden.vercel.app/reminders');
 
-  await page.waitForTimeout(2000);
+  for (let i = 0; i < 10; i++) {
+    await page.getByRole('button', { name: String(time), exact: true }).click();
+    time++;
+    if (time === 12) {
+      time = 1;
+    }
+    await page.getByRole('button', { name: 'Save Reminders' }).click();
+
+    await page.waitForTimeout(2000);
+  }
 }
 
 export { testReminders };
