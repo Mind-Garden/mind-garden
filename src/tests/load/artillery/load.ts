c35e4e77f2f24816f8945a43c18b2e22f671fb2e
Colm Ukrainec <colmukrainec@gmail.com>
2025-03-27 14:58:39 -0500

    fixed comments in artillery load

--- src/tests/load/artillery/load.ts
+++ src/tests/load/artillery/load.ts
@@ -1,11 +1,13 @@
 import { Page } from '@playwright/test';
 
+import { testDataVisualization } from '@/tests/load/commands/test-data-visualization';
 import { testJournal } from '@/tests/load/commands/test-journal';
+import { testLogin } from '@/tests/load/commands/test-login';
 
 async function artilleryScript(page: Page) {
   // comment out tests you dont want running to test your own test
-  // await testLogin(page);
-  // await testDataVisualization(page);
+  await testLogin(page);
+  await testDataVisualization(page);
   await testJournal(page);
 }
 
