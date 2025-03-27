18b455f51973c078ed259743801f52b4b32a2af9
orca277 <killerwhale2779@protonmail.com>
2025-03-27 17:25:22 -0500

    Updated load.ts
    Added testReminders to load.ts and fixed testReminders.ts to press the Reminders button in the dash board instead of the header.

--- src/tests/load/artillery/load.ts
+++ src/tests/load/artillery/load.ts
@@ -2,11 +2,13 @@ import { Page } from '@playwright/test';
 
 import { testDataVisualization } from '@/tests/load/commands/testDataVisualization';
 import { testLogin } from '@/tests/load/commands/testLogin';
+import { testReminders } from '@/tests/load/commands/testReminders';
 
 async function artilleryScript(page: Page) {
   // comment out tests you dont want running to test your own test
   await testLogin(page);
   await testDataVisualization(page);
+  await testReminders(page);
 }
 
 export { artilleryScript };
--- src/tests/load/commands/testReminders.ts
+++ src/tests/load/commands/testReminders.ts
@@ -3,17 +3,16 @@ import { Page } from '@playwright/test';
 let time = 1;
 
 async function testReminders(page: Page) {
-  await page.goto('http://localhost:3000/');
-  await page
-    .locator('section')
-    .filter({ hasText: 'Mind GardenCultivate' })
-    .getByRole('img')
-    .nth(2)
-    .click();
+  await page.goto('https://mindgarden.vercel.app/');
+  await page.waitForTimeout(2000);
+  await page.getByRole('button', { name: 'Get Started' }).click();
   await page.getByRole('textbox', { name: 'Email' }).fill('load@test.com');
   await page.getByRole('textbox', { name: 'Password' }).fill('loadtest');
   await page.getByRole('button', { name: 'Log in' }).click();
-  await page.getByRole('button', { name: 'Reminders', exact: true }).click();
+  await page
+    .getByRole('main')
+    .getByRole('button', { name: 'Reminders' })
+    .click();
   await page.getByRole('button', { name: String(time), exact: true }).click();
   time++;
   if (time === 12) {
