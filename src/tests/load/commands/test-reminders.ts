8c9edc96570665b5618c9fd7119186e9757b3a51
Manisha Gurukumar <ullasmani16@gmail.com>
2025-03-27 21:43:13 -0500

    update reminders test to try again if save button is not visible the first time

--- src/tests/load/commands/test-reminders.ts
+++ src/tests/load/commands/test-reminders.ts
@@ -1,26 +1,42 @@
 import { Page } from '@playwright/test';
 
-let time = 1;
-
 async function testReminders(page: Page) {
-  const workerNumber = Math.floor(Math.random() * 10);
-  const email = `load${workerNumber}@test.com`;
+  const testID = Math.floor(Math.random() * 20);
+  const email = `load${testID}@test.com`;
 
-  await page.goto('https://mindgarden.vercel.app/');
-  await page.waitForTimeout(2000);
+  await page.goto('https://mindgarden.vercel.app/'); // change to localhost:3000 for local testing
   await page.getByRole('button', { name: 'Get Started' }).click();
   await page.getByRole('textbox', { name: 'Email' }).fill(email);
   await page.getByRole('textbox', { name: 'Password' }).fill('loadtest');
   await page.getByRole('button', { name: 'Log in' }).click();
-  await page.goto('https://mindgarden.vercel.app/reminders');
+  await page.waitForURL('https://mindgarden.vercel.app/home');
 
   for (let i = 0; i < 10; i++) {
-    await page.getByRole('button', { name: String(time), exact: true }).click();
-    time++;
-    if (time === 12) {
-      time = 1;
+    await page.goto('https://mindgarden.vercel.app/reminders');
+    await page.waitForURL('https://mindgarden.vercel.app/reminders');
+    await page
+      .getByRole('button', {
+        name: String(Math.floor(Math.random() * 12) + 1),
+        exact: true,
+      })
+      .click();
+
+    const saveButton = page.getByRole('button', {
+      name: 'Save Reminders',
+    });
+
+    if (await saveButton.isVisible()) {
+      await saveButton.click();
+    } else {
+      // save button not visible, select new number and try again
+      await page
+        .getByRole('button', {
+          name: String(Math.floor(Math.random() * 12) + 1),
+          exact: true,
+        })
+        .click();
+      await saveButton.click();
     }
-    await page.getByRole('button', { name: 'Save Reminders' }).click();
 
     await page.waitForTimeout(2000);
   }
